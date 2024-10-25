afce3fea2925dc15ae774846b8e18aa50d5fbaaf
x1101 <71503763+DEX-1101@users.noreply.github.com>
2024-10-25 10:31:55 +0700

    Update deps.py

--- deps.py
+++ deps.py
@@ -1,8 +1,8 @@
 import os
 import subprocess
 print("\033[34mInstalling xformers...\033[0m")
-subprocess.run("pip install xformers==0.0.28.post1", shell=True)
+subprocess.run("pip install xformers==0.0.28.post2", shell=True)
 
 print("\033[34mFixing dependencies...\033[0m")
 subprocess.run("pip install prodigyopt==1.0 onnxruntime==1.17.3", shell=True)
-subprocess.run("pip install flax==0.8.4 jax==0.4.23 jaxlib==0.4.23 opencv-python-headless==4.9.0.80", shell=True)
+subprocess.run("pip install flax==0.8.4 jax==0.4.23 jaxlib==0.4.23 opencv-python-headless", shell=True)
