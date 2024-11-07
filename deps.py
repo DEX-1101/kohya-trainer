1a46299a346ba4ae9238cedf212a57f0e8c5a9f9
x1101 <71503763+DEX-1101@users.noreply.github.com>
2024-11-07 14:27:16 +0700

    Update deps.py

--- deps.py
+++ deps.py
@@ -8,5 +8,4 @@ subprocess.run("pip install prodigyopt==1.0 onnxruntime==1.17.3", shell=True)
 subprocess.run("pip install flax==0.8.4 jax==0.4.23 jaxlib==0.4.23 opencv-python-headless==4.9.0.80 accelerate==0.21.0 huggingface-hub transformers==4.41.0", shell=True)
 
 print("\033[34mChecking dependencies...\033[0m")
-print("\033[1;33m⚠️ " + "\033[1;33m⚠️ ".join(subprocess.run(['pip', 'check'], capture_output=True, text=True).stdout.splitlines()) + "\033[0m") if subprocess.run(['pip', 'check'], capture_output=True, text=True).stdout else print("No dependency issues found.")
-
+result = subprocess.run(['pip', 'check'], capture_output=True, text=True); print("\n".join([f"\033[1;33m {line}\033[0m" for line in result.stdout.splitlines()]) if result.stdout else "No dependency issues found.")
