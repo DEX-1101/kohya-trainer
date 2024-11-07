422406f4ad5c228a64bea6e54d0b89827d95a945
x1101 <71503763+DEX-1101@users.noreply.github.com>
2024-11-07 18:32:13 +0700

    Update deps.py

--- deps.py
+++ deps.py
@@ -5,14 +5,10 @@ subprocess.run("pip install xformers==0.0.28.post2", shell=True)
 
 print("\033[34mFixing dependencies...\033[0m")
 subprocess.run("pip install prodigyopt==1.0 onnxruntime==1.17.3", shell=True)
-subprocess.run("pip install flax==0.8.4 jax==0.4.23", shell=True)
+subprocess.run("pip install flax==0.8.4 jax==0.4.23 accelerate==0.21.0 transformers==4.41.0 opencv-python-headless", shell=True)
 #subprocess.run("", shell=True)
 #subprocess.run("pip install huggingface-hub==0.20.3", shell=True)
 subprocess.run("pip uninstall -y pygobject", shell=True)
 
 print("\033[34mChecking dependencies...\033[0m")
 result = subprocess.run(['pip', 'check'], capture_output=True, text=True); print("\n".join([f"\033[1;33m- {line}\033[0m" for line in result.stdout.splitlines()]) if result.stdout else "No dependency issues found.")
-
-print("\033[34mRestarting Colab Runtime...\033[0m")
-print("\033[34m*proceed to next cell after restarted!\033[0m")
-os.kill(os.getpid(), 9)
