dd815eae2daee4a55aace5a2de482caa1f7101b6
x1101 <71503763+DEX-1101@users.noreply.github.com>
2024-11-07 16:49:55 +0700

    Update deps.py

--- deps.py
+++ deps.py
@@ -6,7 +6,7 @@ subprocess.run("pip install xformers==0.0.28.post2", shell=True)
 print("\033[34mFixing dependencies...\033[0m")
 subprocess.run("pip install prodigyopt==1.0 onnxruntime==1.17.3", shell=True)
 subprocess.run("pip install flax==0.8.4 jax==0.4.23 jaxlib==0.4.23", shell=True)
-subprocess.run("pip install opencv-python-headless==4.9.0.80 accelerate==0.21.0 transformers==4.26.0", shell=True)
+subprocess.run("pip install opencv-python-headless==4.9.0.80 accelerate==0.19.0 transformers==4.26.0", shell=True)
 subprocess.run("pip install huggingface-hub==0.20.3", shell=True)
 #subprocess.run("pip uninstall -y salesforce-lavis pygobject", shell=True)
 
