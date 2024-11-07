1cce58a1bd39993badc201879871f641db3342c5
x1101 <71503763+DEX-1101@users.noreply.github.com>
2024-11-07 18:57:04 +0700

    Update deps.py

--- deps.py
+++ deps.py
@@ -5,7 +5,7 @@ subprocess.run("pip install xformers==0.0.28.post2", shell=True)
 
 print("\033[34mFixing dependencies...\033[0m")
 subprocess.run("pip install prodigyopt==1.0 onnxruntime==1.17.3", shell=True)
-subprocess.run("pip install flax==0.8.4 jax==0.4.23", shell=True)
+subprocess.run("pip install flax==0.8.4 jax==0.4.23 jaxlib==0.4.23 opencv-python-headless", shell=True)
 #subprocess.run("", shell=True)
 #subprocess.run("pip install huggingface-hub==0.20.3", shell=True)
 #subprocess.run("pip uninstall -y pygobject", shell=True)
