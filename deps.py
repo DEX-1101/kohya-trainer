675923c7f8976b18aaf0a32fd8245e951865a54d
x1101 <71503763+DEX-1101@users.noreply.github.com>
2024-11-07 19:38:32 +0700

    Update deps.py

--- deps.py
+++ deps.py
@@ -5,10 +5,9 @@ subprocess.run("pip install -q xformers==0.0.28.post2", shell=True, stdout=subpr
 
 print("\033[34mFixing dependencies...\033[0m")
 subprocess.run("pip -q install prodigyopt==1.0 onnxruntime==1.17.3", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-subprocess.run("pip install -q flax==0.8.4 jax==0.4.23 jaxlib==0.4.23", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+subprocess.run("pip install -q flax==0.8.4 jax==0.4.23 jaxlib==0.4.23 opencv-python-headless", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 subprocess.run("pip install accelerate==0.21.0 huggingface-hub==0.26.2 transformers==4.46.2 opencv-python-headless==4.9.0.80", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 subprocess.run("pip uninstall -y salesforce-lavis pygobject", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 print("\033[34mChecking requirements...\033[0m")
 result = subprocess.run(['pip', 'check'], capture_output=True, text=True); print("\n".join([f"\033[1;33m- {line}\033[0m" for line in result.stdout.splitlines()]) if result.stdout else "No dependency issues found.")
-
